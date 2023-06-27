15de296e41c1d0cc9659f99dcf0ec4400047c78f
Shyam Raghuwanshi <94217498+shyamraghu@users.noreply.github.com>
2023-06-27 05:00:10 -0800

    Update delete.js

--- script/delete.js
+++ script/delete.js
@@ -3,7 +3,14 @@ const allTodoContainer = document.getElementById("allTodoContainer")
 const nullOrNot = document.querySelector('.nullOrNot')
 let disable_Button = document.getElementById('btn')
 let showDeleted = document.querySelector('.showDeleted')
-if (Object.keys(deletedTodos).length ==0  ? nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Deleted :- No Todos</span>` : nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Deleted Todos </span>`)
+    if(deletedTodos){
+  if (Object.keys(todoObj).length == 0 ? nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- No Todos</span>` : nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- </span>`)
+    }
+else{
+  nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- No Todos</span>`
+}
+
+  
 
   // if (deleteTodo) {
   for (const key in deletedTodos) {
