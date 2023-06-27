aff62e959172a1e78269377a773b13fa996b6a7f
Shyam Raghuwanshi <94217498+shyamraghu@users.noreply.github.com>
2023-06-27 04:57:28 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,7 +4,13 @@ let todoObj = JSON.parse(localStorage.getItem('todoObj'))
 const nullOrNot = document.getElementById('nullOrNot')
 let disable_Button = document.getElementById('btn')
 let showDeleted = document.querySelector('.showDeleted')
-if (Object.keys(todoObj).length == 0 ? nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- No Todos</span>` : nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- </span>`)
+if(todoObj){
+  if (Object.keys(todoObj).length == 0 ? nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- No Todos</span>` : nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- </span>`)
+}
+else{
+  nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">MY Todos :- No Todos</span>`
+}
+
 
   for (const key in todoObj) {
 
