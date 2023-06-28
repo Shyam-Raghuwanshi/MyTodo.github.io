bb4be0f9b27f0fa322cfee59acae9627a53747e3
Shyam Raghuwanshi <94217498+shyamraghu@users.noreply.github.com>
2023-06-27 20:02:42 -0800

    Update edit.js

--- script/edit.js
+++ script/edit.js
@@ -75,6 +75,21 @@ const showErrorFun = (msg) => {
     </div>
     </div>`
 }
+const showSuccessFun = (msg) => {
+  showSuccessFun.innerHTML += `<div class="bg-green-600 forNone border-t-4 border-teal-500 rounded-b text-black-900 px-4 py-3 shadow-md" role="alert">
+   <div class="flex">
+      <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20">
+          <path
+            d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+        </svg></div>
+      <div>
+        <p class="font-bold">Success</p>
+        <p class="text-sm">${msg}</p>
+      </div>
+    </div>
+    </div>`
+}
 
 const renderTodo = (key, keyObj) => {
   if (key == UrlKey) {
@@ -109,6 +124,10 @@ const renderTodo = (key, keyObj) => {
             delete getAllPulblishTodo[key]
             localStorage.setItem('todoObj', JSON.stringify(getAllPulblishTodo))
             location.href = '/'
+            showSuccessFun('Item added to Draft')
+            setTimeout(() => {
+              showSuccessFun.innerHTML = ''
+            }, 4000);
           }
           else {
             showErrorFun("This title is already declared")
@@ -123,6 +142,10 @@ const renderTodo = (key, keyObj) => {
           delete getAllPulblishTodo[key]
           localStorage.setItem('todoObj', JSON.stringify(getAllPulblishTodo))
           location.href = '/'
+           showSuccessFun('Item added to Draft')
+            setTimeout(() => {
+              showSuccessFun.innerHTML = ''
+            }, 4000);
         }
 
 
@@ -146,6 +169,10 @@ const renderTodo = (key, keyObj) => {
         getAllPulblishTodo[key] = keyObj
         localStorage.setItem('todoObj', JSON.stringify(getAllPulblishTodo))
         location.href = '/'
+        showSuccessFun('Item Updated')
+            setTimeout(() => {
+              showSuccessFun.innerHTML = ''
+            }, 4000);
       }
 
     })
