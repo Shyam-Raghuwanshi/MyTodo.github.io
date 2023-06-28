15834aeed2ff77a6930c2254da8085b0260f653f
Shyam Raghuwanshi <94217498+shyamraghu@users.noreply.github.com>
2023-06-27 19:55:24 -0800

    Update draft.js

--- script/draft.js
+++ script/draft.js
@@ -3,12 +3,26 @@ let allTodoContainer = document.getElementById("allTodoContainer")
 let todoDraftObj = JSON.parse(localStorage.getItem('todoDraft'))
 const nullOrNot = document.querySelector('.nullOrNot')
 let disable_Button = document.getElementById('btn')
-
+let showSuccessFun = document.querySelector('.showSuccessFun')
 if (!todoDraftObj) {
   nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Draft :- No Todos</span>`
 }
 else if (Object.keys(todoDraftObj).length == 0 ? nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Draft :- No Todos</span>` : nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Draft Todos </span>`)
-
+const showSuccessFun = (msg) => {
+  showError.innerHTML += `<div class="bg-green-600 forNone border-t-4 border-teal-500 rounded-b text-black-900 px-4 py-3 shadow-md" role="alert">
+   <div class="flex">
+      <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20">
+          <path
+            d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+        </svg></div>
+      <div>
+        <p class="font-bold">Error</p>
+        <p class="text-sm">${msg}</p>
+      </div>
+    </div>
+    </div>`
+}
 for (const key in todoDraftObj) {
   let maketodo = `<div class="p-4 lg:w-1/3">
     <div
@@ -48,6 +62,8 @@ Array.from(deleteTodo).forEach((element) => {
     console.log(getTodosFromDraft);
     localStorage.setItem('todoDraft', JSON.stringify(getTodosFromDraft))
     location.reload()
+    showSuccessFun('Success! One item deleted')
+    
   })
 })
 
@@ -62,6 +78,7 @@ Array.from(publishTodo).forEach((element) => {
     delete getTodosFromDraft[e.target.id]
     localStorage.setItem('todoDraft', JSON.stringify(getTodosFromDraft))
     location.reload()
+    showSuccessFun('Success! One item Added to My Todos')
   })
 })
 
