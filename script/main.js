4d7ba6b3a7a57795ae18d2699848176369b8429b
Shyam Raghuwanshi <94217498+shyamraghu@users.noreply.github.com>
2023-06-27 20:08:21 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,6 +4,7 @@ let todoObj = JSON.parse(localStorage.getItem('todoObj'))
 const nullOrNot = document.getElementById('nullOrNot')
 let disable_Button = document.getElementById('btn')
 let showDeleted = document.querySelector('.showDeleted')
+
 if (!todoObj) {
   nullOrNot.innerHTML += `<span  class="text-[2rem]  font-bold">Draft :- No Todos</span>`
 }
@@ -49,16 +50,7 @@ const deleteTodoBtn = document.getElementsByClassName('deleteBtn')
 let getDeleteTodos = JSON.parse(localStorage.getItem("deletedTodos"))
 Array.from(deleteTodoBtn).forEach((e) => {
   e.addEventListener('click', ((e) => {
-    showDeleted.innerHTML += `<div class="bg-indigo-900 text-center py-4 lg:px-4">
-    <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-    <span class="flex cursor-pointer rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Success</span>
-    <span class="font-semibold mr-2 text-left flex-auto">One item successfully deleted.</span>
-      <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-    </div>
-  </div>`
-    setTimeout(() => {
-      showDeleted.innerHTML = ''
-    }, 5000);
+   
     let deleteObj = {}
     let obj = todoObj[e.target.name]
     deleteObj[e.target.name] = obj
@@ -69,14 +61,20 @@ Array.from(deleteTodoBtn).forEach((e) => {
       getDeleteTodos[e.target.name] = obj
       localStorage.setItem('deletedTodos', JSON.stringify(getDeleteTodos))
     }
-    //  deleteObj[]
 
     delete todoObj[e.target.name]
     localStorage.setItem('todoObj', JSON.stringify(todoObj))
     location.reload()
+     showDeleted.innerHTML += `<div class="bg-indigo-900 text-center py-4 lg:px-4">
+    <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+    <span class="flex cursor-pointer rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Success</span>
+    <span class="font-semibold mr-2 text-left flex-auto">One item successfully deleted.</span>
+      <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+    </div>
+  </div>`
+    setTimeout(() => {
+      showDeleted.innerHTML = ''
+    }, 5000);
   }))
 })
 
-// let todoObj = {
-//   96.65010056054993: { 'shyam bhai': "is the best in the world" }
-// }
